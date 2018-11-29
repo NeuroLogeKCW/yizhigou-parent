87f6dd2f3ca32c48c59f0e5481b7442e8a46bc4a
Joshua Kong <kcw78917@gmail.com>
2018-11-29 23:18:46 +0800

    完成了                      1、商家管理除了广告管理的几乎全部功能                      2、完成了修改商品信息
    完成了
      1、运营商广告
      2、客户端广告

---
+++ yizhigou-service-provider/src/main/java/com/yizhigou/content/service/impl/ContentServiceImpl.java
@@ -0,0 +1,121 @@
+package com.yizhigou.content.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.yizhigou.content.service.ContentService;
+import com.yizhigou.entity.PageResult;
+import com.yizhigou.mapper.TbContentMapper;
+import com.yizhigou.pojo.TbContent;
+import com.yizhigou.pojo.TbContentExample;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class ContentServiceImpl implements ContentService {
+
+	@Autowired
+	private TbContentMapper contentMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbContent> findAll() {
+		return contentMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbContent content) {
+		contentMapper.insert(content);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbContent content){
+		contentMapper.updateByPrimaryKey(content);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbContent findOne(Long id){
+		return contentMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			contentMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbContentExample example=new TbContentExample();
+		TbContentExample.Criteria criteria = example.createCriteria();
+		
+		if(content!=null){			
+						if(content.getTitle()!=null && content.getTitle().length()>0){
+				criteria.andTitleLike("%"+content.getTitle()+"%");
+			}
+			if(content.getUrl()!=null && content.getUrl().length()>0){
+				criteria.andUrlLike("%"+content.getUrl()+"%");
+			}
+			if(content.getPic()!=null && content.getPic().length()>0){
+				criteria.andPicLike("%"+content.getPic()+"%");
+			}
+			if(content.getStatus()!=null && content.getStatus().length()>0){
+				criteria.andStatusLike("%"+content.getStatus()+"%");
+			}
+	
+		}
+		
+		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	@Override
+	public List<TbContent> findByCategoryId(Long categoryId) {
+
+		TbContentExample tbContentExample = new TbContentExample();
+		TbContentExample.Criteria criteria = tbContentExample.createCriteria();
+		//categorid为指定值，必须是已开启的
+		criteria.andCategoryIdEqualTo(categoryId).andStatusEqualTo("1");
+		tbContentExample.setOrderByClause("sort_order");
+		List<TbContent> tbContents = contentMapper.selectByExample(tbContentExample);
+		return tbContents;
+	}
+
+}
